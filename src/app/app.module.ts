4d06b5c9502fcc1b8e5d92e7baaae9bd9d07a973
Darshan M Hegde <hegdes123@gmail.com>
2022-08-07 00:23:10 +0530

    Angular week 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,24 @@ import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     FlexLayoutModule,
   ],
   providers: [],
