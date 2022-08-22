3be2223db22a8b1d2889f78619e54251762d2ee6
Darshan M Hegde <hegdes123@gmail.com>
2022-08-22 22:25:41 +0530

    #2 Angular week 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,9 +16,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent, AboutComponent, HomeComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
